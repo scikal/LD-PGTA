2c0beeb58f14ca446077a1fb94389340307525ba
Daniel Ariad <daniel@ariad.org>
2021-04-14 22:59:37 -0400

    Daily build

--- MISC/CHECK_RATE_OF_HETEROZYGOSITY.py
+++ MISC/CHECK_RATE_OF_HETEROZYGOSITY.py
@@ -7,35 +7,49 @@ Created on Thu Jun  4 18:51:19 2020
 
 @author: ariad
 """
-import collections, operator, os, pickle, itertools
+import collections, operator, os, pickle, itertools, gzip
 
-def read_impute2(impute2_filename,**kwargs):
-    """ Reads an IMPUTE2 file format (SAMPLE/LEGEND/HAPLOTYPE) and builds a list
+def read_impute2(filename,**kwargs):
+    """ Reads an IMPUTE2 file format (LEGEND/HAPLOTYPE/SAMPLE) and builds a list
         of lists, containing the dataset. """
     
     filetype = kwargs.get('filetype', None)
-    with open(impute2_filename, 'r') as impute2_in:
-        if filetype == 'leg':
+    
+    def leg_format(line):
+        rs_id, pos, ref, alt = line.strip().split()
+        return ('chr'+rs_id[:2].rstrip(':'), int(pos), ref, alt)  
+    
+    def sam_format(line):
+          sample_id, group1, group2, sex = line.strip().split(' ')
+          return (sample_id, group1, group2, int(sex))  
+      
+    with (gzip.open(filename,'rt') if filename[-3:]=='.gz' else open(filename, 'r')) as impute2_in:
+        if filetype == 'leg': 
             impute2_in.readline()   # Bite off the header
-            def parse(x): 
-                y=x.strip().split()
-                y[0] = 'chr'+y[0].split(':')[0]
-                y[1]=int(y[1])
-                return tuple(y)
+            result = tuple(map(leg_format,impute2_in))
+            
         elif filetype == 'hap':
-            def parse(x):
-                return tuple(i=='1' for i in x.strip().split())
+            firstline = impute2_in.readline()   # Get first line
+            a0 = int(firstline.replace(' ', ''), 2)
+            a1 = (int(line.replace(' ', ''), 2) for line in impute2_in)
+            hap_tab = (a0, *a1)
+            number_of_haplotypes = len(firstline.strip().split())
+            result = hap_tab, number_of_haplotypes
+            
+        elif filetype == 'sam': 
+            impute2_in.readline()   # Bite off the header
+            result = tuple(map(sam_format,impute2_in))
+        
         else:
-            def parse(x):
-                return tuple(x.strip().split()) # Trailing whitespaces stripped from the ends of the string. Then it splits the string into a list of words.
-       
-        impute2_tab = tuple(parse(line) for line in impute2_in)
-    return impute2_tab
+            result = tuple(line.strip().split() for line in impute2_in)
+    
+    return result 
 
-def build_hap_dict(leg_tab,hap_tab):
-    N = len(hap_tab[0])
-    hap_dict = {pos: tuple(hap_tab[ind]) 
-                     for ind,(chr_id, pos, ref, alt) in enumerate(leg_tab) if 0.05<hap_tab[ind].count(True)/N<0.95}
+
+def build_hap_dict(leg_tab,hap_tab,number_of_haplotypes):
+    N = number_of_haplotypes
+    hap_dict = {pos: hap
+                     for (chr_id, pos, ref, alt), hap in zip(leg_tab,hap_tab) if 0.05<bin(hap).count('1')/N<0.95}
     
     return hap_dict
 
@@ -67,26 +81,24 @@ def check_var(hap_tab):
     c = np.sum(C)/len(C)/len(A)
     return (b0,b,c)
 
-def check_var2(hap_dict):
-    from operator import ne
-    N = len(next(iter(hap_dict.values())))//2
-    result = {pos: sum(map(ne,hap[1::2],hap[0::2]))/N for pos,hap in hap_dict.items()}
+def check_var2(hap_dict,number_of_haplotypes):
+    N = number_of_haplotypes/2
+    result = {pos: bin(hap ^ (hap >> 1))[2::2].count('1')/N for pos,hap in hap_dict.items()}
     return result
 
-def check_var3(hap_dict):
-    N = len(next(iter(hap_dict.values())))//3
-    C = lambda *x: not (all(x) or not any(x))
-    result = {pos: sum(map(C,hap[0::3],hap[1::3],hap[2::3]))/N for pos,hap in hap_dict.items()}
+def check_var3(hap_dict,number_of_haplotypes):
+    N = number_of_haplotypes/3
+    result = {pos: bin( (hap ^ (hap >> 1)) | (hap ^ (hap >> 2)) | ((hap >> 1) ^ (hap >> 2)) )[2::3].count('1')/N for pos,hap in hap_dict.items()}
     return result
     
 if __name__=='__main__':
-    leg_filename = '../build_reference_panel/ref_panel.EUR.hg38.BCFtools/chr21_EUR_panel.legend'
-    hap_filename = '../build_reference_panel/ref_panel.EUR.hg38.BCFtools/chr21_EUR_panel.hap'
+    leg_filename = '../../build_reference_panel/EUR_panel.hg38.BCFtools/chr21_EUR_panel.legend.gz'
+    hap_filename = '../../build_reference_panel/EUR_panel.hg38.BCFtools/chr21_EUR_panel.hap.gz'
     leg_tab = read_impute2(leg_filename,filetype='leg')
-    hap_tab = read_impute2(hap_filename,filetype='hap')
-    hap_dict = build_hap_dict(leg_tab,hap_tab)
-    A = check_var2(hap_dict)
-    B = check_var3(hap_dict)
-    from matplotlib import pyplot
-    pyplot.scatter(A.keys(),[1/i if i else -10 for i in A.values()],s=1)
-    pyplot.scatter(B.keys(),[1/i if i else -10 for i in B.values()],s=1)
+    hap_tab, number_of_haplotypes = read_impute2(hap_filename,filetype='hap')
+    hap_dict = build_hap_dict(leg_tab,hap_tab,number_of_haplotypes)
+    A = check_var2(hap_dict,number_of_haplotypes)
+    B = check_var3(hap_dict,number_of_haplotypes)
+    #from matplotlib import pyplot
+    #pyplot.scatter(A.keys(),[1/i if i else -10 for i in A.values()],s=1)
+    #pyplot.scatter(B.keys(),[1/i if i else -10 for i in B.values()],s=1)
