04b6149a3a1e3de94f44d99ec7bbffecb27d49cc
Daniel Ariad <daniel@ariad.org>
2021-08-20 00:19:49 -0400

    Daily build

--- MISC/SIMULATED_SEQUENCES_PIPELINE.py
+++ MISC/SIMULATED_SEQUENCES_PIPELINE.py
@@ -8,16 +8,14 @@ Daniel Ariad (daniel@ariad.org)
 Dec 30, 2020
 
 """
-import time, pickle, re, sys, random, sys
+import time, sys, random, os, operator
 from random import sample, choices, seed
 from multiprocessing import Process
 
-from os import remove
-import os
-
 sys.path.append('../')
 from MIX_HAPLOIDS import MixHaploids_wrapper
-from IMPUTE2OBS import main as simulate
+from IMPUTE2OBS import main as simulate_IMPUTE2based
+from VCF2OBS import main as simulate_VCFbased
 
 def read_ref(filename):
     with open(filename, 'r') as data_in:
@@ -42,6 +40,7 @@ def runInParallel(*fns,**kwargs):
             None
 
 def transitions(chr_id):
+    """ Generates transitions between BPH to SPH regions for trisomy of meiosis II origin. """
     x = int(chr_id[3:]) if chr_id[3:].isnumeric() else chr_id[3:]
     if type(x) is int and 1<=x<=6:
         #BPH-SPH-BPH-SPH
@@ -56,12 +55,12 @@ def transitions(chr_id):
         result = ('SPH',1)
     return (result,)
 
-def aneuploidy_test_demo(obs_filename,chr_id,sp,model,min_reads,max_reads,output_dir):
+def aneuploidy_test_demo(obs_filename,chr_id,sp,model,min_reads,max_reads,output_dir,complex_admixture):
     from ANEUPLOIDY_TEST import aneuploidy_test
     args = dict(obs_filename = f'results_{sp:s}/ABC.obs.p',
-                hap_filename = f'../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.hap.gz',
-                leg_filename = f'../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend.gz',
-                sam_filename = f'../build_reference_panel/samples_per_panel/{sp:s}_panel.samples',
+                hap_filename = f'../../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.hap.gz',
+                leg_filename = f'../../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend.gz',
+                sam_filename = f'../../build_reference_panel/samples_per_panel/{sp:s}_panel.samples',
                 window_size = 0,
                 subsamples = 100,
                 offset = 0,
@@ -74,68 +73,104 @@ def aneuploidy_test_demo(obs_filename,chr_id,sp,model,min_reads,max_reads,output
                 compress = 'bz2')
                 #model = model)
     args['obs_filename'] = obs_filename #f'results_{sp:s}/' + obs_filename
+    if complex_admixture:
+        args['ancestral_proportion'] = (sp[:3],0.5)
     LLR_dict, info = aneuploidy_test(**args)
     return LLR_dict, info
 
-#def make_simulated_obs_tab(sample_id,sp,chr_id,genotypes,output_dir):
-#    """ Based on VCF2OBS """
-#    bcftools_dir = '' #'../bcftools-1.10.2/bin'
-#    #sample_id = 'HG00096'
-#    #chr_id = 'chr21'
-#    leg_filename = f'../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend'
-#    vcf_filename = f'../vcf_phase3_hg38_v2/ALL.{chr_id:s}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz'
-#    #output_dir  = f'results_{sp:s}/'
-#    return simulate(vcf_filename,leg_filename,chr_id,sample_id,bcftools_dir,genotypes=genotypes,output_dir=output_dir)
+def make_simulated_obs_tab_VCFbased(sample_id,sp,chr_id,genotypes,output_dir):
+    """ Based on VCF2OBS """
+    bcftools_dir = '' #'../bcftools-1.10.2/bin'
+    #sample_id = 'HG00096'
+    #chr_id = 'chr21'
+    leg_filename = f'../../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend.gz'
+    vcf_filename = f'../../vcf_phase3_hg38_v2/ALL.{chr_id:s}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz'
+    #output_dir  = f'results_{sp:s}/'
+    return simulate_VCFbased(vcf_filename,leg_filename,chr_id,sample_id,bcftools_dir,genotypes=genotypes,output_dir=output_dir)
 
-def make_simulated_obs_tab(sample_id,sp,chr_id,genotypes,output_dir):
+def make_simulated_obs_tab_IMPUTE2based(sample_id,sp,chr_id,genotypes,output_dir):
     """ Based on IMPUTE2OBS """
-    path = '../build_reference_panel/'
+    path = '../../build_reference_panel/'
     #path = f'../build_reference_panel/ref_panel.{sp:s}.hg38.BCFtools/'
     leg_filename = path + f'{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend.gz'
     hap_filename = path + f'{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.hap.gz'
     sam_filename = path + f'samples_per_panel/{sp:s}_panel.samples'
-    return simulate(leg_filename,hap_filename,sam_filename,chr_id,sample_id,genotypes=genotypes,output_dir=output_dir)
+    return simulate_IMPUTE2based(leg_filename,hap_filename,sam_filename,chr_id,sample_id,genotypes=genotypes,output_dir=output_dir)
 
-def main(depth,sp,chr_id,read_length,min_reads,max_reads):
-    ###depth = 0.5
-    ###sp = 'EUR'
-    ###chr_id = 'chr21'
-    work_dir = f"/mybox/simulations/results_{sp:s}" #'results_EAS' #
+def main(depth,sp,chr_id,read_length,min_reads,max_reads,work_dir,complex_admixture=False):
+    work_dir = work_dir.rstrip('/') + '/' if len(work_dir)!=0 else ''
+    #####################
+    SPsorted = {('EUR_EAS'): 'EAS_EUR',
+                ('EAS_EUR'): 'EAS_EUR',
+                ('EUR_SAS'): 'SAS_EUR',
+                ('SAS_EUR'): 'SAS_EUR',
+                ('EAS_SAS'): 'EAS_SAS',
+                ('SAS_EAS'): 'EAS_SAS',
+                ('AFR_EUR'): 'AFR_EUR',
+                ('EUR_AFR'): 'AFR_EUR',
+                'EUR': 'EUR',
+                'EAS': 'EAS',
+                'SAS': 'SAS',
+                'AMR': 'AMR',
+                'AFR': 'AFR'}
     #####################
     seed(None, version=2)
-    work_dir = work_dir.rstrip('/') + '/' if len(work_dir)!=0 else ''
-    INDIVIDUALS = read_ref(f"../build_reference_panel/samples_per_panel/{sp:s}_panel.txt") #EAS_panel.txt')
-    A = sample(INDIVIDUALS,k=3)
-    B = choices(['A','B'],k=3)
+    list_SP = sp.split('_')
+    
+    if len(list_SP)==1:
+        INDIVIDUALS = read_ref(f"../../build_reference_panel/samples_per_panel/{sp:s}_panel.txt") #EAS_panel.txt')
+        A = sample(INDIVIDUALS,k=3)
+        B = choices(['A','B'],k=3)
+    elif len(list_SP)==2 and not complex_admixture:
+        B = choices(['A','B'],k=3)
+        A = []
+        for p in list_SP:
+            INDIVIDUALS = read_ref(f"../../build_reference_panel/samples_per_panel/{p:s}_panel.txt") #EAS_panel.txt')
+            A.extend(sample(INDIVIDUALS,k=2))
+        A = sample(A,k=3)
+    elif len(list_SP)==2 and complex_admixture:
+        B = choices(['A','B'],k=6)
+        A = []
+        for p in list_SP:
+            INDIVIDUALS = read_ref(f"../../build_reference_panel/samples_per_panel/{p:s}_panel.txt") #EAS_panel.txt')
+            A.extend(sample(INDIVIDUALS,k=3))
+        A = operator.itemgetter(0,3,1,4,2,5)(A)
+    else:
+        print('error: unsupported sp value.')
+    
     C = [i+j for i,j in zip(A,B)]
-    #for a,b in zip(A,B): make_simulated_obs_tab(a,sp,chr_id,b,work_dir)
-    func = (eval(f"lambda: make_simulated_obs_tab('{a:s}', '{sp:s}', '{chr_id:s}', '{b:s}', '{work_dir:s}')") for a,b in zip(A,B))
-    runInParallel(*func)
-    filenames = MixHaploids_wrapper(f'{work_dir:s}{C[0]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[1]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[2]:s}.{chr_id:s}.hg38.obs.p', read_length=read_length, depth=depth, scenarios=('monosomy','disomy','SPH','BPH','transitions'), transitions=transitions(chr_id),output_dir=work_dir)
-
+    print(C)
+    #####################
+    
+    for a,b in zip(A,B): make_simulated_obs_tab_IMPUTE2based(a, SPsorted[sp], chr_id, b, work_dir) 
+    sim_obs_tabs = [f'{work_dir:s}{c:s}.{chr_id:s}.hg38.obs.p' for c in C]
+    filenames = MixHaploids_wrapper(*sim_obs_tabs, read_length=read_length, depth=depth, scenarios=('SPH','BPH'),
+                                    output_dir=work_dir, complex_admixture=complex_admixture)
     for c in C: os.remove(f'{work_dir:s}{c:s}.{chr_id:s}.hg38.obs.p')
-
-    #filenames = ["/home/ariad/Dropbox/postdoc_JHU/origin_ecosystem/origin_V2/results_EUR/simulated.SPH.chr21.x0.010.NA20536B.NA20536A.obs.p",
-    #             "/home/ariad/Dropbox/postdoc_JHU/origin_ecosystem/origin_V2/results_EUR/simulated.BPH.chr21.x0.010.NA20536B.NA20536A.NA20802B.rs0.00.obs.p",
-    #             "/home/ariad/Dropbox/postdoc_JHU/origin_ecosystem/origin_V2/results_EUR/simulated.monosomy.chr21.x0.010.NA20536B.obs.p",
-    #             "/home/ariad/Dropbox/postdoc_JHU/origin_ecosystem/origin_V2/results_EUR/simulated.disomy.chr21.x0.010.NA20536B.NA20536A.obs.p"]
     print(filenames)
-    func2 = (eval(f"lambda: aneuploidy_test_demo('{f:s}','{chr_id:s}','{sp:s}','MODELS/MODELS16.p',{min_reads:d},{max_reads:d},'{work_dir:s}')") for f in filenames)
-    runInParallel(*func2)
-    #for f in filenames: aneuploidy_test_demo(f,chr_id,sp,'MODELS/MODELS16.p',min_reads,max_reads,work_dir)
+    for f in filenames: aneuploidy_test_demo(f,chr_id,SPsorted[sp],'MODELS/MODELS16.p',
+                                             min_reads,max_reads,work_dir, complex_admixture)
     return 0
 
 
 if __name__ == "__main__":
+    random.seed(a=None, version=2)
+    complex_admixture=True
     depth=0.05
-    sp='EUR'
+    ###sp='EUR_AFR'
     chr_id='chr21'
-    read_length = 250
+    read_length = 36
     min_reads,max_reads = 18,8
-    for n in ([*range(1,23)]+['X']):
-        #chr_id = 'chr' + str(n)
-        runInParallel(*([main]*6),args=(depth,sp,chr_id,read_length,min_reads,max_reads) )
-    #main(depth,sp,chr_id,read_length,min_reads,max_reads)
+    
+    #for n in ([*range(1,23)]+['X']):
+    #    chr_id = 'chr' + str(n)
+    #    runInParallel(*([main]*12),args=(depth,sp,chr_id,read_length,min_reads,max_reads,work_dir) )
+    #main(depth,sp,chr_id,read_length,min_reads,max_reads,work_dir,complex_admixture)
+    for i in range(10):
+        print(i)
+        for sp in ('AFR_EUR','EAS_SAS','SAS_EUR','EAS_EUR'):
+            work_dir = f"/mybox/C-simulations/results_{sp:s}" #'../results' #'results_EAS' 
+            runInParallel(*([main]*30),args=(depth,sp,chr_id,read_length,min_reads,max_reads,work_dir,complex_admixture) )
     print('DONE.')
     pass
 else:
